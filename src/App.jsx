338036ef72a468d1d4635c0e7945fae48204081e
vmarialuzm <vmarialuzm@gmail.com>
2025-03-07 17:37:42 -0500

    llamados a apis con fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import StaticComponent from "./componentes/StaticComponent/StaticComponent";
+import UserList from "./componentes/UserList/UserList";
 
 function App() {
   return (
     <div>
       <StaticComponent />
+      <UserList />
     </div>
   );
 }
---
+++ src/componentes/UserList/UserList.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(data => setUsers(data))
+            .catch(error => console.error('Error fetching data:',error));
+    },[]);
+
+    return (
+        <div>
+            <h1>Usuarios</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default UserList;
