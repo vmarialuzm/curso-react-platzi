f6e9dbc256eb59be4409a5c80b73444cd3d89aa4
vmarialuzm <vmarialuzm@gmail.com>
2025-03-10 10:49:36 -0500

    Uso de context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import Counter from "./componentes/Counter/Counter";
+import { useContext, createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => prevTheme === "light" ? "dark" : "light")
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <button
+      onClick={toggleTheme}
+      style={{
+        backgroundColor: theme === "light" ? "#fff" : "#000",
+        color: theme === "light" ? "#000" : "#fff",
+      }}
+    >
+      Cambiar tema
+    </button>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Counter />
+      <ThemeProvider>
+        <ThemeButton />
+      </ThemeProvider>
     </div>
   );
 }
